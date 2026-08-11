4d2012acb4d6172d2efbe8aa255e048e7255d0fc
PenguWoodGanon <maxwellj2022@icloud.com>
2026-08-11 13:09:08 -0600

    Finished Prototype

--- Godot - Prototype/Scripts/player.gd
+++ Godot - Prototype/Scripts/player.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
-
 const SPEED = 500.0
 const JUMP_VELOCITY = -500.0
 
+@onready var sprite_2d = $Sprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
+	#Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	#Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#Left/Right
 	var direction = Input.get_axis("left", "right")
+
+	#Flip
+	if direction > 0:
+		sprite_2d.flip_h = false
+	elif direction < 0:
+		sprite_2d.flip_h = true
+
+	# Movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
