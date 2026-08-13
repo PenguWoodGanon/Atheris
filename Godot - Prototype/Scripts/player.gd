a395cb00b65c63e7cf5ec89fec89b4ddf02ea2f1
PenguWoodGanon <maxwellj2022@icloud.com>
2026-08-13 15:22:31 -0600

    Dash Added to Prototype
    I just added a simple dash, maybe it might need fixing so you can't rapidly dash, probably is_touching_floor(). E to Dash

--- Godot - Prototype/Scripts/player.gd
+++ Godot - Prototype/Scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 500.0
+var SPEED = 500.0
 const JUMP_VELOCITY = -500.0
 
 @onready var sprite_2d = $Sprite2D
+@onready var dashtimer = $dashtimer
 
 func _physics_process(delta):
 	#Gravity
@@ -16,7 +17,13 @@ func _physics_process(delta):
 
 	#Left/Right
 	var direction = Input.get_axis("left", "right")
-
+	
+	#Dash
+	if Input.is_action_just_pressed("dash"):
+		dashtimer.start()
+		SPEED *= 8
+		velocity.x = direction * SPEED
+	
 	#Flip
 	if direction > 0:
 		sprite_2d.flip_h = false
@@ -30,3 +37,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_dashtimer_timeout():
+	#Reset Speed
+	SPEED = 500.0
