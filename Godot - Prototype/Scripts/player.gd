b945049c0d4bdb54feb0109cd4224d3661afaec0
PenguWoodGanon <maxwellj2022@icloud.com>
2026-08-16 12:01:13 -0600

    Finished Rex

--- Godot - Prototype/Scripts/player.gd
+++ Godot - Prototype/Scripts/player.gd
@@ -4,7 +4,7 @@ var SPEED = 500.0
 const JUMP_VELOCITY = -500.0
 var dashcount = 0
 
-@onready var sprite_2d = $Sprite2D
+@onready var sprite_2d = $AnimatedSprite2D
 @onready var dashtimer = $dashtimer
 
 func _physics_process(delta):
@@ -17,18 +17,27 @@ func _physics_process(delta):
 	#Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("shoot"):
+		sprite_2d.play("shoot")
 
 	#Left/Right
 	var direction = Input.get_axis("left", "right")
+	if Input.is_action_just_pressed("left"):
+		sprite_2d.play("run")
+	if Input.is_action_just_pressed("right"):
+		sprite_2d.play("run")
 	
 	#Dash
 	if Input.is_action_just_pressed("dash"):
 		if is_on_floor():
+			sprite_2d.play("dash")
 			dashtimer.start()
 			SPEED *= 8
 			velocity.x = direction * SPEED
 			print(dashcount)
 		elif not is_on_floor() and dashcount == 0:
+			sprite_2d.play("dash")
 			dashcount += 1
 			dashtimer.start()
 			SPEED *= 8
@@ -41,6 +50,8 @@ func _physics_process(delta):
 		sprite_2d.flip_h = false
 	elif direction < 0:
 		sprite_2d.flip_h = true
+	elif direction == 0:
+		sprite_2d.play("idle")dd
 
 	# Movement
 	if direction:
@@ -52,5 +63,6 @@ func _physics_process(delta):
 
 #Reset Speed
 func _on_dashtimer_timeout():
+	sprite_2d.play("default")
 	SPEED = 500.0
 	
